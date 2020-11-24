7c7fb7d1c679433c64df459ee94e5ad47af23180
David Greving <david.greving@gmail.com>
2020-11-25 00:26:14 +0100

    all bonuses meetup_date

--- morsels/meetup_date/meetup_date.py
+++ morsels/meetup_date/meetup_date.py
@@ -1,13 +1,33 @@
 from datetime import date, timedelta
 
 
+class Weekday:
+    MONDAY = 0
+    TUESDAY = 1
+    WEDNESDAY = 2
+    THURSDAY = 3
+    FRIDAY = 4
+    SATURDAY = 5
+    SUNDAY = 6
+
+
 def meetup_date(y, m, nth=4, weekday=3):
-    dt = date(y, m, 1)
+    if nth > 0:
+        dt = date(y, m, 1)
+    else:
+        dt = date(y, (m + 1) % 12, 1) - timedelta(days=1)
     while dt.weekday() != weekday:
-        dt += timedelta(days=1)
-    return dt + timedelta(weeks=nth-1)
+        if nth > 0:
+            dt += timedelta(days=1)
+        else:
+            dt -= timedelta(days=1)
+    if nth > 0:
+        return dt + timedelta(weeks=nth-1)
+    else:
+        return dt + timedelta(weeks=nth+1)
 
 
 if __name__ == '__main__':
-    rv = meetup_date(2007, 7)
+    rv = meetup_date(2007, 7, nth=-1)
     print(rv.weekday())
+    print(Weekday.MONDAY)
